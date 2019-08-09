4e805c4e7a539e41a0addd4e298ed266331a4a1d
jordanlesson <jordantreylesson@gmail.com>
2019-08-08 22:17:30 -0400

    First commit

---
+++ alpaca_trading.py
@@ -0,0 +1,127 @@
+import alpaca_trade_api as alpaca
+import requests
+import json
+import time
+import asyncio
+
+alpaca_api = alpaca.REST(
+    key_id = 'PKHOZ4HY9LW966N7YVYS',
+    secret_key = 'sJ29MQ4Bgf8XlYrxu73GYsatmH1kWLX5oqKwGbvH',
+    base_url = 'https://paper-api.alpaca.markets',
+)
+
+
+def main():
+    # done = None
+    print("Start Running")
+    while True:
+        clock = alpaca_api.get_clock()
+        now = clock.timestamp
+        if clock.is_open:
+            # Do Algorithm Here
+            stock_info = asyncio.run(get_spread('GOOG', 'GOOGL'))
+
+            class_a_info = stock_info[0]
+            class_b_info = stock_info[1]
+
+            spread = abs(class_a_info["latestPrice"] - class_b_info["latestPrice"])
+
+            if spread is not None:
+                average_spread = asyncio.run(get_historical_data('GOOG', 'GOOGL'))
+
+                upper_bound = average_spread + (0.25 * average_spread)
+                lower_bound = average_spread - (0.25 * average_spread)
+
+                if spread > upper_bound or spread < lower_bound:
+                    hedge_position(class_a_info, class_b_info)
+
+        time.sleep(0.333)
+
+
+async def get_spread(class_a, class_b):
+    try:
+        class_a_api = 'https://cloud.iexapis.com/stable/stock/{}/quote?token=sk_a31d816214bb42f7902c3c63abe6909b'.format(class_a)
+        class_b_api = 'https://cloud.iexapis.com/stable/stock/{}/quote?token=sk_a31d816214bb42f7902c3c63abe6909b'.format(class_b)
+
+        class_a_response = requests.get(class_a_api)
+        class_b_response = requests.get(class_b_api)
+
+        if class_a_response.status_code == 200 and class_b_response.status_code == 200:
+
+            class_a_data = json.loads(class_a_response.text)
+
+            class_b_data = json.loads(class_b_response.text)
+
+            class_a_info = {
+                "symbol": class_a_data["symbol"],
+                "latestPrice": class_a_data["latestPrice"],
+                "bid": class_a_data["iexBidPrice"],
+                "ask": class_a_data["iexAskPrice"],
+            }
+
+            class_b_info = {
+                "symbol": class_b_data["symbol"],
+                "latestPrice": class_b_data["latestPrice"],
+                "bid": class_b_data["iexBidPrice"],
+                "ask": class_b_data["iexAskPrice"],
+            }
+
+            # print(f'{class_a}: {class_a_price}')
+            # print(f'{class_b}: {class_b_price}')
+            # print(f'Spread: {abs(class_a_price - class_b_price)}')
+            return [class_a_info, class_b_info]
+
+        else:
+
+            print("Error Retrieving Stock Price")
+            return None
+
+    except IOError:
+
+        print(IOError)
+        return None
+
+
+async def get_historical_data(class_a, class_b):
+    try:
+
+        class_a_avg = 'https://cloud.iexapis.com/stable/stock/{}/stats/day200MovingAvg/?token=sk_a31d816214bb42f7902c3c63abe6909b'.format(class_a)
+        class_b_avg = 'https://cloud.iexapis.com/stable/stock/{}/stats/day200MovingAvg/?token=sk_a31d816214bb42f7902c3c63abe6909b'.format(class_b)
+
+        class_a_response = requests.get(class_a_avg)
+        class_b_response = requests.get(class_b_avg)
+
+        if class_a_response.status_code == 200 and class_b_response.status_code == 200:
+
+            class_a_data = json.loads(class_a_response.text)
+            class_b_data = json.loads(class_b_response.text)
+
+            class_a_avg_price = float(class_a_data)
+            class_b_avg_price = float(class_b_data)
+
+            avg_spread = abs(class_a_avg_price - class_b_avg_price)
+
+            return avg_spread
+
+        else:
+
+            print('Error Retrieving Average Spread')
+            return None
+
+    except IOError:
+
+        print(IOError)
+        return None
+
+
+async def hedge_position(class_a_info, class_b_info):
+
+    if class_a_info["latestPrice"] > class_b_info["latestPrice"]:
+        alpaca_api.submit_order(class_a_info["symbol"], 5, 'sell', 'market', 'day')
+        alpaca_api.submit_order(class_b_info["symbol"], 5, 'buy', 'market', 'day')
+    else:
+        alpaca_api.submit_order(class_b_info["symbol"], 5, 'sell', 'market', 'day')
+        alpaca_api.submit_order(class_a_info["symbol"], 5, 'buy', 'market', 'day')
+
+if __name__ == "__main__":
+    main()
